8b1cbc09979fb04dc3e208061471af3579af6d43
mikespub <github@mikespub.net>
2021-10-19 19:03:36 +0000

    show environ

--- app.py
+++ app.py
@@ -1,13 +1,36 @@
 import os
-from flask import Flask
+import socket
+import sys
+import time
+
+from flask import Flask, escape, json, request
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return 'Hello World!'
 
-if __name__ == '__main__':
+@app.route("/")
+def hello():
+    # return 'Hello World!'
+    info = json.dumps(request.environ, indent=2, default=lambda o: repr(o))
+    tmpl = (
+        "<h3>Hello {name}!</h3>"
+        "<b>Hostname:</b> {hostname}<br/>"
+        "<b>File:</b> {file}<br/>"
+        "<b>Modified:</b> {date}<br/>"
+        "<b>Python:</b> {version}<br/>"
+        "<b>Environ:</b> <pre>{environ}</pre>"
+    )
+    return tmpl.format(
+        name=os.getenv("NAME", "World"),
+        hostname=socket.gethostname(),
+        file=__file__,
+        date=time.ctime(os.path.getmtime(__file__)),
+        version=escape(sys.version),
+        environ=escape(info),
+    )
+
+
+if __name__ == "__main__":
     # Bind to PORT if defined, otherwise default to 5000.
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
